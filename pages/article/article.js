2af57e5372da4a3726c89df6c7bbfbd4ad14ae90
Oliver <linfeng.ou@gmail.com>
2018-07-10 09:02:01 +0800

    init ver.

---
+++ pages/article/article.js
@@ -0,0 +1,65 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    link: 'https://mp.weixin.qq.com/s?__biz=MzI0NzQ0ODY3NA==&mid=2247483689&idx=1&sn=cedbaf87e13063ffd2a8e8f4623bb80e&chksm=e9ae9424ded91d32a8920d92320dd5dc17ce97512820148d038841db1da039b33064dfd5d074#rd'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
